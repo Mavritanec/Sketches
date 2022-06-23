c1e4efc7f5a703eac4c0fb5ac8d0dc874f9e14c6
sesamema <sesamema@student.21-school.ru>
2022-06-24 00:24:11 +0300

    Added sketches
    Add 1, 2, 3, 4 sketches.
    
    Add information about them in readme (only russian part).

---
+++ sketch_002.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+#define BUFF_SIZE 64
+
+
+int main() {
+    int code[] = {99, 100, 101, 69, 102, 103, 71, 105,
+                  110, 111, 112, 115, 117, 120, 88, 37};
+
+    for (int i = 0; i < 16; i++) printf("%d:\t%c\n", code[i], code[i]);
+
+    return 0;
+}
---
+++ sketch_003.c
@@ -0,0 +1,29 @@
+#include <locale.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define BUFF_SIZE 64
+
+
+int main() {
+    setlocale(LC_ALL, "");
+
+    char tmp[BUFF_SIZE];
+    char ch[BUFF_SIZE / 16];
+    wchar_t w_str[] = L"森b我e爱菠萝";
+
+    int w_str_len = wcstombs(tmp, w_str, BUFF_SIZE);
+
+    printf("%d\n", w_str_len);
+
+    int i = 0, sum = 0;
+
+    while (*(w_str + i)) {
+        int w_ch_len = wctomb(ch, w_str[i]);
+        sum += w_ch_len;
+        printf("%d %d\n", w_ch_len, sum);
+        i++;
+    }
+
+    return 0;
+}
