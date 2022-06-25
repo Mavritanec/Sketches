96b09965a5bee59bdc5f759fc5f12b738583e522
Sesame Machelle <sesamema@il-f6.msk.21-school.ru>
2022-06-25 14:39:53 +0300

    Added sketch 006
    Macros and definition operation system.

---
+++ sketch_006.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+#if defined(__APPLE__) // || defined(__MACH__)
+#define OS 'M'
+#elif defined(__linux__)
+#define OS 'L'
+#else
+#define OS 'L'
+#endif
+
+
+int main() {
+    printf("Operation system: %c.\n", OS);
+    return 0;
+}
